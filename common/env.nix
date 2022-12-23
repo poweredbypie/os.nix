d5aec18fc4cbf03ed3fd98749e240a92051eee2e
PoweredByPie <poweredbypie@users.noreply.github.com>
2022-12-22 18:09:13 -0800

    Remove elvish from global apps

--- common/env.nix
+++ common/env.nix
@@ -9,7 +9,6 @@
     # Global packages (mostly just handy for root to have)
     systemPackages = with pkgs; [
       kakoune
-      elvish
       usbutils
       # This is required for nixos-rebuild with flakes.
       gitMinimal
