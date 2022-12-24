540ad19cf795300f30f0c9a654c8db5dc22c3816
PoweredByPie <poweredbypie@users.noreply.github.com>
2022-12-23 16:21:13 -0800

    Disable xwayland globally

--- common/sway.nix
+++ common/sway.nix
@@ -6,4 +6,6 @@
   # Annoyingly, since this has some dependency on GL etc,
   # this has to be set here instead of in the home manager config.
   programs.sway.enable = true;
+  # I don't use anything that doesn't natively support wayland
+  programs.xwayland.enable = false;
 }
