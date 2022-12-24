c7717b800f45976c003ceaf32a1be6d786c5683a
PoweredByPie <poweredbypie@users.noreply.github.com>
2022-12-24 12:30:59 -0800

    Prefer dark mode in both GTK 3 and 4

--- common/home/gtk.nix
+++ common/home/gtk.nix
@@ -1,11 +1,14 @@
 # GTK theming
 { ... }:
 
-{
+let
+  dark = {
+    extraConfig.gtk-application-prefer-dark-theme = true;
+  };
+in {
   gtk = {
     enable = true;
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
+    gtk3 = dark;
+    gtk4 = dark;
   };
 }
