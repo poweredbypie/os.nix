c91318661af313a399087ce561c4e9a0df776e3d
PoweredByPie <poweredbypie@users.noreply.github.com>
2022-12-23 16:21:33 -0800

    Add PDF viewer (zathura)

--- common/home/default.nix
+++ common/home/default.nix
@@ -9,6 +9,7 @@
     ./font.nix
     ./kak
     ./sway
+    ./zathura.nix
   ];
 
   # Same as the Nix version, I've heard I shouldn't touch this.
---
+++ common/home/zathura.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [pkgs.zathura];
+
+  programs.zathura = {
+    enable = true;
+    extraConfig = ''
+      # Use the Ctrl+V clipboard or whatever it's called
+      set selection-clipboard clipboard
+    '';
+  };
+}
