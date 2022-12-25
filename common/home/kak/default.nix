13c1401a21bae98b2d10640ec11201d12a202765
PoweredByPie <poweredbypie@users.noreply.github.com>
2022-12-25 14:37:48 -0800

    Fix, annotate, and improve kakoune config

--- common/home/kak/default.nix
+++ common/home/kak/default.nix
@@ -9,6 +9,10 @@
       # Editorconfig implementation
       editorconfig-core-c
     ];
+    # TODO:
+    # - Split things into multiple files by category
+    # - Hook colorscheme to have transparent background
+    # - Look into only formatting spaces on save
     config = {
       colorScheme = "gruvbox-dark";
       hooks = [
@@ -23,7 +27,57 @@
         {
           name = "WinSetOption";
           option = "filetype=(c|cpp)";
-          commands = "lsp-enable-window";
+          commands = ''
+            lsp-enable-window
+            # Enables nicer diagnostics
+            lsp-inlay-diagnostics-enable window
+            lsp-inlay-code-lenses-enable window
+            lsp-inlay-hints-enable window
+
+            # Prefer spaces
+            set-option window lsp_insert_spaces true
+            # Show info inline with cursor (looks nicer)
+            set-option window lsp_hover_anchor true
+
+            # Use semantic highlighting
+            # TODO: This should be configured more Nix-like, please fix!
+            hook window -group semantic-tokens BufReload .* lsp-semantic-tokens
+            hook window -group semantic-tokens NormalIdle .* lsp-semantic-tokens
+            hook window -group semantic-tokens InsertIdle .* lsp-semantic-tokens
+            hook -once -always window WinSetOption filetype=.* %{
+              remove-hooks window semantic-tokens
+            }
+          '';
+        }
+        # Use tab for autocomplete
+        {
+          name = "InsertCompletionShow";
+          option = ".*";
+          commands = ''
+            try %{
+              # I don't even know what this does lol
+              execute-keys -draft 'h<a-K>\h<ret>'
+              map window insert <tab> <c-n>
+              map window insert <s-tab> <c-p>
+              hook -once -always window InsertCompletionHide .* %{
+                unmap window insert <tab> <c-n>
+                unmap window insert <s-tab> <c-p>
+              }
+            }
+          '';
+        }
+      ];
+      keyMappings = [
+        # Indent and dedent with spaces.
+        {
+          key = "<tab>";
+          mode = "insert";
+          effect = "<a-;><a-gt>";
+        }
+        {
+          key = "<s-tab>";
+          mode = "insert";
+          effect = "<a-;><a-lt>";
         }
       ];
       indentWidth = 4;
@@ -39,7 +93,8 @@
     };
     extraConfig = ''
       # Load kak-lsp
-      eval %sh{kak-lsp --kakoune s $kak_session}
+      eval %sh{kak-lsp --kakoune -s $kak_session}
+      lsp-auto-hover-enable
     '';
   };
 }
