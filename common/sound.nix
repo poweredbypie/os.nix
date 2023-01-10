be127e8f71b53597cd0d56c143df050c1ca3f7d3
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-09 18:14:00 -0800

    sound.nix: Add a comment about default sinks and reduce redundancy

--- common/sound.nix
+++ common/sound.nix
@@ -1,11 +1,14 @@
 { ... }:
 
 {
+  # TODO: I have absolutely no idea how to declaratively set
+  # a certain sink in Wireplumber to be the default.
+  # I know I can do it with `wpctl set-default`, but
+  # there has to be a config file where I can set it, right???
+  # Either way, it works right now, so I'm just not going to touch it.
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-    audio.enable = true;
-    wireplumber.enable = true;
   };
 }
