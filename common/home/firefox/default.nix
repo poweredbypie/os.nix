65a071bc6127f85cdf2848063b3889c40964a41c
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-09 18:14:19 -0800

    firefox: Add some useful bookmarks

--- common/home/firefox/default.nix
+++ common/home/firefox/default.nix
@@ -11,6 +11,16 @@
     ];
     profiles.pie = {
       isDefault = true;
+      bookmarks = [
+        {
+          name = "Home Manager options";
+          url = "https://nix-community.github.io/home-manager/options.html";
+        }
+        {
+          name = "NixOS Search";
+          url = "https://search.nixos.org/packages";
+        }
+      ];
       settings = {
         # Use Zathura instead
         "pdfjs.disabled" = true;
