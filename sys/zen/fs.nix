ef3f5fe2aa3693c625709cd839710e72101e5548
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-16 17:56:01 -0800

    zen: Mount hard disk and change docs partition mount name

--- sys/zen/fs.nix
+++ sys/zen/fs.nix
@@ -4,5 +4,24 @@
   # This is really bad!
   # We shouldn't be referring to a home directory as a mountpoint.
   # Potential solution: Make a /docs folder? But make it rwx for the users group?
-  fileSystems."/home/pie/bridge".label = "bridge";
+  fileSystems =
+  let perms = [
+    "uid=pie"
+    "gid=users"
+  ];
+  in {
+    "/home/pie/docs" = {
+      label = "bridge";
+      options = perms;
+    };
+    "/home/pie/seagate" = {
+      label = "seagate";
+      fsType = "ntfs3";
+      options = perms ++ [
+        # `noatime` might be beneficial to load times? Try later
+        # Osu reference??
+        "nofail"
+      ];
+    };
+  };
 }
