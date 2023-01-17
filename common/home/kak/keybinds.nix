f7038247c9064a5853ae76101d5119ead6c56a72
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-17 00:31:33 -0800

    kak: Add more keybinds

--- common/home/kak/keybinds.nix
+++ common/home/kak/keybinds.nix
@@ -13,5 +13,33 @@
       mode = "insert";
       effect = "<a-;><a-lt>";
     }
+    {
+      key = "<tab>";
+      mode = "normal";
+      effect = "<a-gt>";
+    }
+    {
+      key = "<s-tab>";
+      mode = "normal";
+      effect = "<a-lt>";
+    }
+    # VSCode-like navigation while inserting
+    {
+      key = "<c-left>";
+      mode = "insert";
+      effect = "<esc>b;i";
+    }
+    {
+      key = "<c-right>";
+      mode = "insert";
+      effect = "<esc>e;i";
+    }
+    # Ctrl+W deletes a word while inserting
+    # Ctrl+Backspace is mapped to Ctrl+W in my terminal emulator
+    {
+      key = "<c-w>";
+      mode = "insert";
+      effect = "<esc>bdi";
+    }
   ];
 }
