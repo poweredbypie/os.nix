6c9479c93f265fa623285a8ac8ed9f3b32079148
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-16 17:16:18 -0800

    verthe: Split up system-specific files

---
+++ sys/verthe/boot.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+
+{
+  boot = {
+    loader = {
+      # Use systemd boot
+      systemd-boot.enable = true;
+      # I don't actually know what this does in detail but I'm assuming it's fine
+      efi.canTouchEfiVariables = true;
+    };
+    extraModulePackages = with config.boot.kernelPackages; [ rtl8821cu ];
+  };
+}
--- sys/verthe/default.nix
+++ sys/verthe/default.nix
@@ -1,15 +1,8 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
-
-  boot = {
-    loader = {
-      # Use systemd boot
-      systemd-boot.enable = true;
-      # I don't actually know what this does in detail but I'm assuming it's fine
-      efi.canTouchEfiVariables = true;
-    };
-    extraModulePackages = with config.boot.kernelPackages; [ rtl8821cu ];
-  };
+  imports = [
+    ./boot.nix
+    ./hardware-configuration.nix
+  ];
 }
