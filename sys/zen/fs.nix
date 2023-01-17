86b9bd1b0d5ec9a8e7a0b3d593111dd641559308
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-16 17:58:21 -0800

    zen/fs: Run `nix fmt`

--- sys/zen/fs.nix
+++ sys/zen/fs.nix
@@ -5,23 +5,25 @@
   # We shouldn't be referring to a home directory as a mountpoint.
   # Potential solution: Make a /docs folder? But make it rwx for the users group?
   fileSystems =
-  let perms = [
-    "uid=pie"
-    "gid=users"
-  ];
-  in {
-    "/home/pie/docs" = {
-      label = "bridge";
-      options = perms;
-    };
-    "/home/pie/seagate" = {
-      label = "seagate";
-      fsType = "ntfs3";
-      options = perms ++ [
-        # `noatime` might be beneficial to load times? Try later
-        # Osu reference??
-        "nofail"
+    let
+      perms = [
+        "uid=pie"
+        "gid=users"
       ];
+    in
+    {
+      "/home/pie/docs" = {
+        label = "bridge";
+        options = perms;
+      };
+      "/home/pie/seagate" = {
+        label = "seagate";
+        fsType = "ntfs3";
+        options = perms ++ [
+          # `noatime` might be beneficial to load times? Try later
+          # Osu reference??
+          "nofail"
+        ];
+      };
     };
-  };
 }
