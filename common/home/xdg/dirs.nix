0e726bb2d84437276166baed51630761f0acdae0
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-01-21 21:03:14 -0800

    xdg/dirs: Specify all directories

--- common/home/xdg/dirs.nix
+++ common/home/xdg/dirs.nix
@@ -5,13 +5,19 @@
     let
       home = config.home.homeDirectory;
       docs = "${home}/docs";
+      media = "${docs}/media";
     in
     {
       enable = true;
       createDirectories = true;
       desktop = docs;
       documents = docs;
-      download = "${home}/misc";
+      music = media;
+      pictures = media;
+      videos = media;
+      download = "${home}/tmp";
+      publicShare = null;
+      templates = null;
       # TODO: Is this even a good idea???
       extraConfig.XDG_SOURCE_DIR = "${home}/src";
     };
