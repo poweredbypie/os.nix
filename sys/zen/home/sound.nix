0248df6d253ce06f7ddbddf2604e9c8dd1aada1a
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-02-04 17:03:43 -0800

    zen/sound: Use `lib.generators.toINI`
    This is cleaner since it doesn't leave a stray INI file in the Nix store.

--- sys/zen/home/sound.nix
+++ sys/zen/home/sound.nix
@@ -1,16 +1,11 @@
 # Declaratively set the default node in WirePlumber.
 
-{ pkgs, ... }:
+{ lib, ... }:
 
-{
+with lib.generators; {
   # EW?
   # I couldn't get the Lua scripting to work lol
-  home.file.".local/state/wireplumber/default-nodes".source =
-    let
-      toINI = config: (pkgs.formats.ini { }).generate "nodes.ini" config;
-      sink = "alsa_output.pci-0000_05_00.1.HiFi__hw_Generic_3__sink";
-    in
-    toINI {
-      default-nodes."default.configured.audio.sink" = sink;
-    };
+  home.file.".local/state/wireplumber/default-nodes".text = toINI { } {
+    default-nodes."default.configured.audio.sink" = "alsa_output.pci-0000_05_00.1.HiFi__hw_Generic_3__sink";
+  };
 }
