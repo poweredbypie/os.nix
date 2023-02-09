1dac6f883c40a0048f6115d53a05479243efa8d2
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-02-08 18:08:58 -0800

    firefox/apps: Use separate profile for apps
    This allows me to open new Firefox windows without them also starting in kiosk mode.

--- common/home/firefox/apps.nix
+++ common/home/firefox/apps.nix
@@ -2,10 +2,18 @@
 
 { pkgs, ... }:
 
+let
+  profile = "apps";
+in
 {
+  # TODO: Hack; If using the same profile, all other windows open in kiosk mode!
+  programs.firefox.profiles."${profile}" = {
+    id = 1;
+  };
+
   home.packages = let
     inherit (pkgs) writeShellScriptBin;
-    mkApp = name: url: (writeShellScriptBin name "firefox --kiosk --new-window ${url}");
+    mkApp = name: url: (writeShellScriptBin name "firefox -P ${profile} --kiosk --new-window ${url}");
   in
   [
     (mkApp "discord" "https://discord.com/app")
