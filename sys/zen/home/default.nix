eae142685e3737e291d6ebea0d7905b083945e92
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-12-08 07:34:43 -0800

    zen: Add poweralertd and powertop

--- sys/zen/home/default.nix
+++ sys/zen/home/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./power.nix
     ./sound.nix
     ./sway.nix
     ./vpn.nix
--- sys/zen/power.nix
+++ sys/zen/power.nix
@@ -1,5 +1,7 @@
 { ... }:
 
 {
+  services.upower.enable = true;
   powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
 }
