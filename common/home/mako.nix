efabf8cadebcc04085aea269c2802363035721f0
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-12-08 07:51:37 -0800

    common/home/mako: Add timeout of 3 seconds by default

--- common/home/mako.nix
+++ common/home/mako.nix
@@ -4,6 +4,7 @@
   services.mako = {
     enable = true;
     anchor = "bottom-left";
+    defaultTimeout = 3000;
     # TODO: Merge with Sway style options
     backgroundColor = "#000000";
     borderColor = "#000000";
