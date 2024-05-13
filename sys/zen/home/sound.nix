9ab1c12e1653b8cc770aa253d08516b2625d2fb7
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-05-12 21:32:18 -0700

    Set default microphone on zen

--- sys/zen/home/sound.nix
+++ sys/zen/home/sound.nix
@@ -7,5 +7,6 @@ with lib.generators; {
   # I couldn't get the Lua scripting to work lol
   home.file.".local/state/wireplumber/default-nodes".text = toINI { } {
     default-nodes."default.configured.audio.sink" = "alsa_output.pci-0000_05_00.1.HiFi__hw_Generic_3__sink";
+    default-nodes."default.configured.audio.source" = "alsa_input.pci-0000_05_00.6.HiFi__hw_acp__source";
   };
 }
