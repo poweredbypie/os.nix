5e3b7dd1e78e7ec9abe06aa15649c6b6fae36661
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-05-13 17:58:54 -0700

    Rework network setup to use systemd over dhcpcd

--- common/net.nix
+++ common/net.nix
@@ -10,9 +10,19 @@
       settings = {
         # Enable integrated DHCP.
         General.EnableNetworkConfiguration = true;
-        Network.EnableIPv6 = true;
+        Network = {
+          EnableIPv6 = true;
+          NameResolvingService = "systemd";
+        };
         Settings.AutoConnect = true;
       };
     };
+    # Use IWD's integrated service instead.
+    dhcpcd.enable = false;
+    useNetworkd = true;
+  };
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
   };
 }
