3161d53de3fb9dc385762301f0668e6e4bc8a5ac
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-05-29 16:21:27 -0700

    Oops

--- common/home/rnote.nix
+++ common/home/rnote.nix
@@ -4,10 +4,10 @@
   home.packages = [
     # Fix weird crashing with NGL
     # https://github.com/flxzt/rnote/issues/1061
-    pkgs.writeShellScriptBin
-    "rnote"
-    ''
-      GSK_RENDERER=gl ${pkgs.rnote}/bin/rnote
-    ''
+    (pkgs.writeShellScriptBin
+      "rnote"
+      ''
+        GSK_RENDERER=gl ${pkgs.rnote}/bin/rnote
+      '')
   ];
 }
