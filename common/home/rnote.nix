2df578654d0f35a2b350bd6a17aefab7ea0194d7
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-05-29 16:20:33 -0700

    Fix rnote crashing

--- common/home/rnote.nix
+++ common/home/rnote.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.rnote ];
+  home.packages = [
+    # Fix weird crashing with NGL
+    # https://github.com/flxzt/rnote/issues/1061
+    pkgs.writeShellScriptBin
+    "rnote"
+    ''
+      GSK_RENDERER=gl ${pkgs.rnote}/bin/rnote
+    ''
+  ];
 }
