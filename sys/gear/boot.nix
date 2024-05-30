f82f0a8ce8cb58bc0db6cc149c0c4f2e0ac2c38f
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-05-29 20:20:36 -0700

    gear: Blacklist audio card and force use internal mic
    God this took hours to figure out

--- sys/gear/boot.nix
+++ sys/gear/boot.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  # Disable the audio input from the 3.5mm jack (my earpod mic is broken)
+  # https://discourse.nixos.org/t/fixing-audio-on-asus-strix-scar-17-g733qs/12687/2
+  hardware.firmware = [
+    (pkgs.writeTextDir "/lib/firmware/hda-jack-retask.fw" (builtins.readFile ./hda-jack-retask.fw))
+  ];
   boot = {
     loader = {
       # Use systemd boot
@@ -8,5 +13,9 @@
       # I don't actually know what this does in detail but I'm assuming it's fine
       efi.canTouchEfiVariables = true;
     };
+    # Patch the firmware to force the audio input as the internal mic
+    extraModprobeConfig = ''
+      options snd-hda-intel patch=hda-jack-retask.fw
+    '';
   };
 }
--- sys/gear/power.nix
+++ sys/gear/power.nix
@@ -6,7 +6,11 @@
   services.tlp = {
     enable = true;
     settings = {
-      STOP_CHARGE_THRESH_BAT0 = 80;
+      STOP_CHARGE_THRESH_BAT1 = 90;
+      # Disable annoying pop noise when controller goes to sleep
+      # Thanks https://community.frame.work/t/responded-headphone-jack-intermittent-noise/5246/61
+      SOUND_POWER_SAVE_ON_AC = 0;
+      SOUND_POWER_SAVE_ON_BAT = 0;
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
