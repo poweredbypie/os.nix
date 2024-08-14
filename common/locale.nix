00f77f43cfb783ce03be2092b086a60b4859c408
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-08-13 23:25:14 -0700

    I give up with locale

--- common/locale.nix
+++ common/locale.nix
@@ -1,14 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 let
-  suffix = ".UTF-8/UTF-8";
+  suffix = ".UTF-8";
   us = "en_US" + suffix;
   au = "en_AU" + suffix;
 in
 {
   time.timeZone = "America/Los_Angeles";
-  i18n.supportedLocales = [ us au ];
-  i18n.defaultLocale = us;
+  # i18n.supportedLocales = [ us au ];
+  # i18n.defaultLocale = us;
   console.keyMap = "us";
 
   i18n.inputMethod = {
