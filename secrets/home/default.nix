39535e25a179a14f0d33b388e0a427221ab19bbb
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-09-09 15:57:40 -0700

    Add zen's keys

--- secrets/home/default.nix
+++ secrets/home/default.nix
@@ -3,6 +3,12 @@
 let
   inherit (config.pie) host;
   home = config.home.homeDirectory;
+  mkHostKeys = host: {
+    "ssh/${host}/pie" = { };
+    "ssh/${host}/pie.pub" = { };
+    "ssh/${host}/irl" = { };
+    "ssh/${host}/irl.pub" = { };
+  };
 in
 {
   sops = {
@@ -19,11 +25,9 @@ in
           format = "binary";
           sopsFile = ./ssh-irl;
         };
-        "ssh/gear/pie" = { };
-        "ssh/gear/pie.pub" = { };
-        "ssh/gear/irl" = { };
-        "ssh/gear/irl.pub" = { };
       }
+      (mkHostKeys "gear")
+      (mkHostKeys "zen")
       {
         "ssh/${host}/pie" = {
           path = "${home}/.ssh/pie";
@@ -31,6 +35,7 @@ in
         "ssh/${host}/irl" = {
           path = "${home}/.ssh/irl";
         };
+        # These aren't actually secrets but it's just convenient to use sops to deploy them
         "ssh/${host}/pie.pub" = {
           path = "${home}/.ssh/pie.pub";
           mode = "0444";
