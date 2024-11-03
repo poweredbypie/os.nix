d91b0022ca5d78efe7146ee765c123108d525c30
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-11-02 23:39:37 -0700

    gear: Monitor stuff

--- sys/gear/home/sway.nix
+++ sys/gear/home/sway.nix
@@ -2,8 +2,16 @@
   wayland.windowManager.sway.config =
     let
       main = "eDP-1";
+      top = "DP-6";
     in
     {
-      output."${main}".scale = "1.3";
+      output."${main}" = {
+        scale = "1.3";
+        pos = "0 1080";
+      };
+      output."${top}" = {
+        res = "1920x1080@240Hz";
+        pos = "0 0";
+      };
     };
 }
